6be2bf9ab1f5991d24e22bb5d2c53f8ea38bd443
Amrik <ztriplezero@hotmail.com>
2019-10-17 13:08:36 -0400

    all Sprites are being pushed by player : Success!

--- BabaIsYou-F19/GameScene.swift
+++ BabaIsYou-F19/GameScene.swift
@@ -12,23 +12,68 @@ import GameplayKit
 class GameScene: SKScene, SKPhysicsContactDelegate {
     var player:SKSpriteNode! = nil
     var stopblock:SKSpriteNode! = nil
-    let playerSpeed:CGFloat =  20
+    var wallBlock:SKSpriteNode! = nil
+    var flagBlock:SKSpriteNode! = nil
+    var winBlock:SKSpriteNode! = nil
+    var flag:SKSpriteNode! = nil
+    let playerSpeed:CGFloat =  40
 
     override func didMove(to view: SKView) {
         self.physicsWorld.contactDelegate = self
-         self.stopblock = self.scene?.childNode(withName: "stopblock") as! SKSpriteNode
+        self.stopblock = self.scene?.childNode(withName: "stopblock") as! SKSpriteNode
         self.player = self.scene?.childNode(withName: "player") as! SKSpriteNode
+        self.wallBlock = self.scene?.childNode(withName: "wallblock") as! SKSpriteNode
+        self.flagBlock = self.scene?.childNode(withName: "flagblock") as! SKSpriteNode
+        self.winBlock = self.scene?.childNode(withName: "winblock") as! SKSpriteNode
+        self.flag = self.scene?.childNode(withName: "flag") as! SKSpriteNode
         
         self.player.physicsBody = SKPhysicsBody(rectangleOf: player.size)
         self.player.physicsBody?.isDynamic = true
         self.player.physicsBody?.affectedByGravity = false
         self.player.physicsBody?.allowsRotation = false
+         self.player.physicsBody?.categoryBitMask = 1
         
         
         self.stopblock.physicsBody?.isDynamic = true
         self.stopblock.physicsBody = SKPhysicsBody(rectangleOf: stopblock.size)
        self.stopblock.physicsBody?.affectedByGravity = false
         self.stopblock.physicsBody?.allowsRotation = false
+        self.player.physicsBody?.categoryBitMask = 2
+        
+        self.enumerateChildNodes(withName: "isblock") {
+            (node, stop) in
+            let isBlock = node as! SKSpriteNode
+            isBlock.physicsBody = SKPhysicsBody(rectangleOf: isBlock.size)
+            isBlock.physicsBody?.affectedByGravity = false
+            isBlock.physicsBody?.allowsRotation = false
+            isBlock.physicsBody?.isDynamic = true
+            isBlock.physicsBody?.categoryBitMask = 4
+        }
+        
+        self.wallBlock.physicsBody = SKPhysicsBody(rectangleOf: wallBlock.size)
+        self.wallBlock.physicsBody?.isDynamic = true
+        self.wallBlock.physicsBody?.affectedByGravity = false
+        self.wallBlock.physicsBody?.allowsRotation = false
+        self.wallBlock.physicsBody?.categoryBitMask = 8
+        
+        self.winBlock.physicsBody = SKPhysicsBody(rectangleOf: winBlock.size)
+        self.winBlock.physicsBody?.isDynamic = true
+        self.winBlock.physicsBody?.affectedByGravity = false
+        self.winBlock.physicsBody?.allowsRotation = false
+        self.winBlock.physicsBody?.categoryBitMask = 32
+        
+        self.flagBlock.physicsBody = SKPhysicsBody(rectangleOf: flagBlock.size)
+        self.flagBlock.physicsBody?.isDynamic = true
+        self.flagBlock.physicsBody?.affectedByGravity = false
+        self.flagBlock.physicsBody?.allowsRotation = false
+        self.flagBlock.physicsBody?.categoryBitMask = 64
+        
+        self.flag.physicsBody = SKPhysicsBody(rectangleOf: flag.size)
+        self.flag.physicsBody?.isDynamic = true
+        self.flag.physicsBody?.affectedByGravity = false
+        self.flag.physicsBody?.allowsRotation = false
+        self.flag.physicsBody?.categoryBitMask = 128
+
     }
    
     func didBegin(_ contact: SKPhysicsContact) {
